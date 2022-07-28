0ae8c521bedd8c3a02e994f19702dc45e2e5bdb3
ljaewon97 <ljaewon97@gmail.com>
2022-07-28 22:58:42 +0900

    SW 역량테스트 기출 3문제 업로드 20220728

---
+++ src/G3_Ramp_14890.java
@@ -0,0 +1,110 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class G3_Ramp_14890 {
+	static int[][] map;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int ans = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		// 가로 방향
+		for(int r = 0; r < N; r++) {
+			boolean canPass = true;
+			int same = 1;
+			for(int c = 1; c < N; c++) {
+				if(map[r][c-1] + 1 == map[r][c]) {
+					if(same < L) {
+						canPass = false;
+						break;
+					}
+				}
+				else if(map[r][c-1] == map[r][c] + 1) {
+					for(int i = 1; i < L; i++) {
+						int nc = c + i;
+						if(nc < 0 || nc >= N || map[r][nc] != map[r][c]) {
+							canPass = false;
+							break;
+						}
+					}
+					c += L - 1;
+					same = 0;
+					continue;
+				}
+				else if(Math.abs(map[r][c] - map[r][c-1]) > 1) {
+					canPass = false;
+					break;
+				}
+				
+				if(map[r][c-1] == map[r][c]) {
+					same++;
+				}
+				else {
+					same = 1;
+				}
+			}
+			
+			if(canPass) {
+				ans++;
+			}
+		}
+		
+		// 세로 방향
+		for(int c = 0; c < N; c++) {
+			boolean canPass = true;
+			int same = 1;
+			for(int r = 1; r < N; r++) {
+				if(map[r-1][c] + 1 == map[r][c]) {
+					if(same < L) {
+						canPass = false;
+						break;
+					}
+				}
+				else if(map[r-1][c] == map[r][c] + 1) {
+					for(int i = 1; i < L; i++) {
+						int nr = r + i;
+						if(nr < 0 || nr >= N || map[nr][c] != map[r][c]) {
+							canPass = false;
+							break;
+						}
+					}
+					r += L - 1;
+					same = 0;
+					continue;
+				}
+				else if(Math.abs(map[r][c] - map[r-1][c]) > 1) {
+					canPass = false;
+					break;
+				}
+				
+				if(map[r-1][c] == map[r][c]) {
+					same++;
+				}
+				else {
+					same = 1;
+				}
+			}
+			
+			if(canPass) {
+				ans++;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
---
+++ src/S2_StartAndLink_14889.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class S2_StartAndLink_14889 {
+	static int[][] abil;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		boolean[] visited = new boolean[N];
+		abil = new int[N][N];
+		int min = Integer.MAX_VALUE;
+		List<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			list.add(i);
+		}
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				abil[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		List<List<Integer>> comb = combResult(list, visited, 0, N, N/2);
+		
+		for(int i = 0; i < comb.size(); i++) {
+			List<Integer> t1 = comb.get(i);
+			List<Integer> t2 = new ArrayList<>();
+			boolean[] v1 = new boolean[N/2];
+			boolean[] v2 = new boolean[N/2];
+			int a1 = 0, a2 = 0;
+			for(Integer num: list) {
+				if(!t1.contains(num)) {
+					t2.add(num);
+				}
+			}
+			
+			List<List<Integer>> team1 = combResult(t1, v1, 0, N/2, 2);
+			List<List<Integer>> team2 = combResult(t2, v2, 0, N/2, 2);
+			
+			a1 = getTeamScore(team1);
+			a2 = getTeamScore(team2);
+			
+			int diff = Math.abs(a1 - a2);
+			if(diff < min) {
+				min = diff;
+			}
+		}
+		
+		System.out.println(min);
+	}
+	
+	static int getTeamScore(List<List<Integer>> team) {
+		int temp = 0;
+		for(List<Integer> l: team) {
+			temp += getAbil(abil, l.get(0), l.get(1));
+		}
+		return temp;
+	}
+	
+	static int getAbil(int[][] abil, int p1, int p2) {
+		return abil[p1][p2] + abil[p2][p1];
+	}
+	
+	static List<List<Integer>> combResult(List<Integer> list, boolean[] visited, int start, int n, int r) {
+		List<List<Integer>> res = new ArrayList<>();
+		combination(res, list, visited, start, n, r);
+		return res;
+	}
+	
+	static void combination(List<List<Integer>> res, List<Integer> list, boolean[] visited, int start, int n, int r) {
+		if(r == 0) {
+			addToList(res, list, visited, n);
+			return;
+		}
+		
+		for(int i = start; i < n; i++) {
+			visited[i] = true;
+			combination(res, list, visited, i+1, n, r-1);
+			visited[i] = false;
+		}
+	}
+	
+	static void addToList(List<List<Integer>> res, List<Integer> list, boolean[] visited, int n) {
+		List<Integer> temp = new ArrayList<>();
+		for(int i = 0; i < n; i++) {
+			if(visited[i]) {
+				temp.add(list.get(i));
+			}
+		}
+		res.add(temp);
+	}
+	
+}
