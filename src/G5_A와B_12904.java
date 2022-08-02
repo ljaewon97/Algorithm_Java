223ac810d9d6d80881f0a350ce60958ea0dea39a
SSAFY <SSAFY@DESKTOP>
2022-08-02 14:09:21 +0900

    G5 A와B 12904 (구현, 문자열) 업로드

---
+++ src/G5_A와B_12904.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class G5_A와B_12904 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] S = br.readLine().toCharArray();
+		char[] T = br.readLine().toCharArray();
+		int l = 0, r = T.length - 1, dir = 0;
+		
+		while(S.length != (Math.abs(r - l) + 1)) {
+			if(T[r] == 'A') {
+				r += dir == 0 ? -1 : 1;
+			}
+			else if(T[r] == 'B') {
+				r += dir == 0 ? -1 : 1;
+				dir = (dir + 1) % 2;
+				int temp = l;
+				l = r;
+				r = temp;
+			}
+		}
+		
+		int ans = 1;
+		int d = dir == 0 ? 1 : -1;
+		for(int i = l, j = 0; i != r + d; i += d, j++) {
+			if(S[j] != T[i]) {
+				ans = 0;
+				break;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
