77e9f6d420ce0991e5bc9c18e814407e6e3df984
ljaewon97 <ljaewon97@gmail.com>
2022-08-07 02:23:26 +0900

    백트래킹

--- src/boj/g4/G4_09663_N_Queen.java
+++ src/boj/g4/G4_09663_N_Queen.java
@@ -4,22 +4,43 @@ import java.util.Scanner;
 
 public class G4_09663_N_Queen {
 	static int N, ans = 0;
-	static boolean[][] visited;
-	static int[] dr = {-1,-1,0,1,1,1,0,-1};
-	static int[] dc = {0,1,1,1,0,-1,-1,-1};
+	static int[] result;
+	static int len = 0;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 	
-		visited = new boolean[N][N];
+		result = new int[N];
 		
-		
-		
-		
+		recur(0);
 		
 		System.out.println(ans);
-		
 		sc.close();
+	}
+	
+	static void recur(int nth) {
+		if(nth == N) {
+			ans++;
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			boolean check = true;
+			
+			for(int j = 0; j < len; j++) {
+				if(nth == j || i == result[j] || Math.abs(nth - j) == Math.abs(i - result[j])) {
+					check = false;
+					break;
+				}
+			}
+			
+			if(check) {
+				result[nth] = i;
+				len++;
+				recur(nth+1);
+				len--;
+			}
+		}
 	}
 }
