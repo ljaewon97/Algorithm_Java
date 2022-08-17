5a482b6fbceaf22bb85964abccace42227a13c3d
ljaewon97 <ljaewon97@gmail.com>
2022-08-17 23:42:34 +0900

    구현, 백트래킹

--- src/boj/g2/G2_17825_주사위_윷놀이.java
+++ src/boj/g2/G2_17825_주사위_윷놀이.java
@@ -3,9 +3,6 @@ package boj.g2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class G2_17825_주사위_윷놀이 {
@@ -34,11 +31,8 @@ public class G2_17825_주사위_윷놀이 {
 			scoreboard[103+i] = 25 + 5 * i;
 			scoreboard[154+i] = 25 + 5 * i;
 		}
-		
 
 		recur(1, new int[] {nums[0], 0, 0, 0}, scoreboard[nums[0]]);
-		
-//		recur(0, new int[4], 0);
 		
 		System.out.println(ans);
 	}
@@ -61,7 +55,7 @@ public class G2_17825_주사위_윷놀이 {
 			boolean flag = false;
 			
 			for(int j = 0; j < 4; j++) {
-				if(horses[j] == next) {
+				if(horses[j] == next || checkSame(horses[j], next)) {
 					flag = true;
 					break;
 				}
@@ -89,5 +83,21 @@ public class G2_17825_주사위_윷놀이 {
 	
 	static boolean finished(int i) {
 		return i != 0 && scoreboard[i] == 0;
+	}
+	
+	static boolean checkSame(int a, int b) {
+		if(54 <= a && a <= 57) a -= 54;
+		else if(103 <= a && a <= 106) a -= 103;
+		else if(154 <= a && a <= 157) a -= 154;
+		else if(a == 20) a = 3;
+		else return false;
+		
+		if(54 <= b && b <= 57) b -= 54;
+		else if(103 <= b && b <= 106) b -= 103;
+		else if(154 <= b && b <= 157) b -= 154;
+		else if(b == 20) b = 3;
+		else return false;
+		
+		return a == b;
 	}
 }
