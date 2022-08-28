81e4279e9c010e63920cce931d1c42b084e39187
ljaewon97 <ljaewon97@gmail.com>
2022-08-28 23:22:52 +0900

    삼성 SW 역량테스트 연습문제

--- src/swea/SWEA_연습문제_15173_Shuffle_O_Matic.java
+++ src/swea/SWEA_연습문제_15173_Shuffle_O_Matic.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class SWEA_연습문제_15173_Shuffle_O_Matic {
-	static int[] cards, result;
+	static int[] cards, result, temp;
 	static int N, ans;
 	
 	public static void main(String[] args) throws IOException {
@@ -43,62 +43,59 @@ public class SWEA_연습문제_15173_Shuffle_O_Matic {
 	}
 	
 	static void recur(int nth) {
-		if(nth == 5) {
-			int[] temp = cards.clone();
-			
-			for(int i = 1; i <= 5; i++) {
-				if(i > ans) return;
-				
-				temp = shuffle(temp, result[i-1]);
-				if(check(temp)) {
-					ans = Math.min(ans, i);
-					return;
-				}
+		if(nth >= ans) return;
+		
+		temp = cards.clone();
+		
+		for(int i = 1; i <= nth; i++) {
+			shuffle(result[i-1]);
+			if(check(temp)) {
+				ans = Math.min(ans, nth);
+				return;
 			}
-			
-			return;
 		}
 		
-		for(int i = 0; i < N; i++) {
+		if(nth == 5) return;
+		
+		for(int i = 1; i < N; i++) {
+			if(nth > 0 && result[nth-1] == N-1 && i == N-1) continue;
 			result[nth] = i;
 			recur(nth+1);
 		}
 	}
 	
-	static int[] shuffle(int[] arr, int x) {
-		int[] res = new int[arr.length];
-		int idx = 0, l = 0, r = arr.length / 2;
+	static void shuffle(int x) {
+		int[] arr = temp.clone();
+		int idx = 0, l = 0, half = arr.length / 2, r = half;
 		
 		if(x < r) {
-			for(int i = 0; i < arr.length / 2 - 1 - x; i++) {
-				res[idx++] = arr[l++];
+			for(int i = 0; i < half - 1 - x; i++) {
+				temp[idx++] = arr[l++];
 			}
 			
-			while(l < arr.length / 2 && r < arr.length) {
-				res[idx++] = arr[l++];
-				res[idx++] = arr[r++];
+			while(l < half && r < arr.length) {
+				temp[idx++] = arr[l++];
+				temp[idx++] = arr[r++];
 			}
 			
 			while(r < arr.length) {
-				res[idx++] = arr[r++];
+				temp[idx++] = arr[r++];
 			}
 		}
 		else {
-			for(int i = 0; i < x - arr.length / 2; i++) {
-				res[idx++] = arr[r++];
+			for(int i = 0; i < x - half; i++) {
+				temp[idx++] = arr[r++];
 			}
 			
-			while(l < arr.length / 2 && r < arr.length) {
-				res[idx++] = arr[r++];
-				res[idx++] = arr[l++];
+			while(l < half && r < arr.length) {
+				temp[idx++] = arr[r++];
+				temp[idx++] = arr[l++];
 			}
 			
-			while(l < arr.length / 2) {
-				res[idx++] = arr[l++];
+			while(l < half) {
+				temp[idx++] = arr[l++];
 			}
 		}
-		
-		return res;
 	}
 	
 	static boolean check(int[] arr) {
@@ -107,8 +104,9 @@ public class SWEA_연습문제_15173_Shuffle_O_Matic {
 		for(int i = 0; i < arr.length-1; i++) {
 			if(arr[i] > arr[i+1]) flagAsc = false;
 			if(arr[i] < arr[i+1]) flagDesc = false;
+			if(!flagAsc && !flagDesc) return false;
 		}
 		
-		return flagAsc || flagDesc;
+		return true;
 	}
 }
