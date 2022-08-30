88343f538b4eba9f90c21bfe18665261f7b6908e
ljaewon97 <ljaewon97@gmail.com>
2022-08-31 01:04:04 +0900

    스터디 주석 추가

--- src/boj/s1/S1_02156_포도주_시식.java
+++ src/boj/s1/S1_02156_포도주_시식.java
@@ -36,12 +36,16 @@ public class S1_02156_포도주_시식 {
 		dp[2][1] = wines[2] + wines[0];
 		dp[2][2] = wines[2];
 		
+		// 0열: i-1번째 포도주를 마시고 i번째 포도주를 마시는 경우
+		// 1열: i-2번째 포도주를 마시고 i번째 포도주를 마시는 경우
+		// 2열: i-3번째 포도주를 마시고 i번째 포도주를 마시는 경우
 		for(int i = 3; i < N; i++) {
-			dp[i][0] = wines[i] + Math.max(dp[i-1][1], dp[i-1][2]);
+			dp[i][0] = wines[i] + Math.max(dp[i-1][1], dp[i-1][2]); // dp i-1행의 0열을 선택하면 연속으로 3잔을 마시게 됨
 			dp[i][1] = wines[i] + Math.max(dp[i-2][0], Math.max(dp[i-2][1], dp[i-2][2]));
 			dp[i][2] = wines[i] + Math.max(dp[i-3][0], Math.max(dp[i-3][1], dp[i-3][2]));
 		}
 		
+		// 뒤에서 두번째, 세번째 포도주를 마시는 경우가 최대일 수 있어서 N-2행까지 확인
 		int m1 = Math.max(dp[N-2][0], Math.max(dp[N-2][1], dp[N-2][2]));
 		int m2 = Math.max(dp[N-1][0], Math.max(dp[N-1][1], dp[N-1][2]));
 		
