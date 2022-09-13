16897b7847fa9a2cba3354893c434bec4b424239
ljaewon97 <ljaewon97@gmail.com>
2022-09-13 21:19:50 +0900

    수학

---
+++ src/boj/b5/B5_10872_팩토리얼.java
@@ -0,0 +1,46 @@
+package boj.b5;
+
+public class B5_10872_팩토리얼 {
+	static Reader in = new Reader();
+	
+	public static void main(String[] args) throws Exception {
+		int N = in.nextInt();
+		int[] fact = {1,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,479001600};
+		System.out.println(fact[N]);
+	}
+	
+	static class Reader {
+		final int SIZE = 5;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws Exception {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-' ? true : false;
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15);
+			while (isNumber(c = read()));
+			if (neg)
+				return -n;
+			return n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws Exception {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
