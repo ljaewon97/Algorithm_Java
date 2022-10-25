5d518264a7b35a57a903be63a1aeb881a04635d9
이재원 <ljaewon97@gmail.com>
2022-10-25 13:31:29 +0900

    구현

---
+++ src/boj/b5/B5_05338_마이크로소프트_로고.java
@@ -0,0 +1,11 @@
+package boj.b5;
+
+public class B5_05338_마이크로소프트_로고 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-;;-._|\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-''-._|");
+	}
+}
