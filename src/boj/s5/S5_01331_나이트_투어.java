9bebcd334ddddb20cf51ed2c87ed02d41e90f4c3
이재원 <ljaewon97@gmail.com>
2022-12-04 14:28:02 +0900

    파일 이름 변경

--- src/boj/s5/S5_01331_나이트_투어.java
+++ src/boj/s5/S5_01331_나이트_투어.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class S5_01331_a {
+public class S5_01331_나이트_투어 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
