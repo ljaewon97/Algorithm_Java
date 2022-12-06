d0484420859e7ff6027892119e853e23bf65e28a
이재원 <ljaewon97@gmail.com>
2022-12-07 01:43:32 +0900

    BigInteger

---
+++ src/boj/b5/B5_10757_큰수_A_plus_B.java
@@ -0,0 +1,18 @@
+package boj.b5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class B5_10757_큰수_A_plus_B {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+		
+		System.out.println(A.add(B).toString());
+	}
+}
