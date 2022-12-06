c2bfe16361680af97002a371d36423a3b73a7cfc
이재원 <ljaewon97@gmail.com>
2022-12-06 17:29:26 +0900

    출력

---
+++ src/boj/b5/B5_05339_콜센터.java
@@ -0,0 +1,7 @@
+package boj.b5;
+
+public class B5_05339_콜센터 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");
+	}
+}
