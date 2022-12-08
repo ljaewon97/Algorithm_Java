dd79dc19ccccd91e11146cf64acd81f39b031337
이재원 <ljaewon97@gmail.com>
2022-12-08 11:41:31 +0900

    DP

---
+++ src/boj/s2/S2_11048_이동하기.java
@@ -0,0 +1,48 @@
+package boj.s2;
+
+public class S2_11048_이동하기 {
+	static Reader in = new Reader();
+	
+	public static void main(String[] args) throws Exception {
+		int N = in.nextInt();
+		int M = in.nextInt();
+		
+		int[][] dp = new int[N+1][M+1];
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= M; j++) {
+				dp[i][j] = Math.max(Math.max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + in.nextInt();
+			}
+		}
+		
+		System.out.println(dp[N][M]);
+	}
+	
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws Exception {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32);
+			do n = (n << 3) + (n << 1) + (c & 15);
+			while (isNumber(c = read()));
+			return n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws Exception {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
