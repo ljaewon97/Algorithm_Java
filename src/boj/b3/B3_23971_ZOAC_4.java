29c0106c5d5fda916cf5f296dfc46a549ba52e8b
이재원 <ljaewon97@gmail.com>
2022-12-09 16:44:17 +0900

    사칙연산

---
+++ src/boj/b3/B3_23971_ZOAC_4.java
@@ -0,0 +1,20 @@
+package boj.b3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B3_23971_ZOAC_4 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		System.out.println(((H-1)/(N+1)+1) * ((W-1)/(M+1)+1));
+	}
+}
